5fe1e5e3bb84b5022d51a174b1d1e84f4a524939
John Warren <john.warren@kobaltmusic.com>
2018-01-21 17:53:03 +0000

    added gallery
    - gallery is now populated with any files found in static/images/collection folder

--- goat_app/views.py
+++ goat_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+import os
+from goat_website import settings
 
 # Create your views here.
 def index(request):
     return render(request, 'goat_app/index.html')
 
 def gallery(request):
-    return render(request, 'goat_app/gallery.html')
+    images = os.listdir(os.path.join(settings.STATIC_DIR, "images/collection/"))
+    return render(request, 'goat_app/gallery.html', context={'images': images})
 
 def about(request):
     return render(request, 'goat_app/about.html')
