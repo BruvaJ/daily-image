166ed517515efc707f7eaa9bd0f06176833341e7
John Warren <john.warren@kobaltmusic.com>
2018-01-21 16:49:04 +0000

    Basic navigation implemented
    - navbar
    - content on all pages
    - simple brand
    - reference to source of brand

--- goat_app/urls.py
+++ goat_app/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from goat_app import views
 
+# TEMPLATE TAGGING
+app_name = 'goat_app'
+
 urlpatterns = [
-    url(r'^$', views.index, name='home')
+    url(r'^$', views.index, name='index'),
+    url(r'^gallery/', views.gallery, name='gallery'),
+    url(r'^about/', views.about, name='about'),
 ]
--- goat_app/views.py
+++ goat_app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'goat_app/index.html')
+
+def gallery(request):
+    return render(request, 'goat_app/gallery.html')
+
+def about(request):
+    return render(request, 'goat_app/about.html')
