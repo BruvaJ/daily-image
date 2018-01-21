59b5b65b31918417823cf87096fc543c3cb8569a
John Warren <john.warren@kobaltmusic.com>
2018-01-21 15:24:07 +0000

    Create first template
    - templates added
    - first basic app ready to load
    - fixed renamings missed in first run

--- goat_website/wsgi.py
+++ goat_website/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for goatWebsite project.
+WSGI config for goat_website project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "goatWebsite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "goat_website.settings")
 
 application = get_wsgi_application()
